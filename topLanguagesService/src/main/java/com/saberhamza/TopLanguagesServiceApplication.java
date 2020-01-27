fd454ec44eb45584aacd9f16db777364e91117c3
saberhamza <saberhamza3@gmail.com>
2020-01-27 20:00:18 +0100

    Add TopLanguesService app which returns the top 100 languages in github

---
+++ topLanguagesService/src/main/java/com/saberhamza/TopLanguagesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.saberhamza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopLanguagesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopLanguagesServiceApplication.class, args);
+	}
+
+}
