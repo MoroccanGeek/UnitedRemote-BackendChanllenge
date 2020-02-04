d54df695d4bbf9c62f0ce89adc2ae52b5beda437
saberhamza <saberhamza3@gmail.com>
2020-02-04 09:53:14 +0100

    Add JavaDoc

--- top100GithubRepos/src/main/java/com/saberhamza/Top100GithubReposApplication.java
+++ top100GithubRepos/src/main/java/com/saberhamza/Top100GithubReposApplication.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * 
+ * The main class to start the application.
+ * 
+ * @author Hamza SABER
+ *
+ */
 @SpringBootApplication
 @EnableEurekaClient
 public class Top100GithubReposApplication {
--- top100GithubRepos/src/main/java/com/saberhamza/config/AppDispatcherServletInitializer.java
+++ top100GithubRepos/src/main/java/com/saberhamza/config/AppDispatcherServletInitializer.java
@@ -2,18 +2,35 @@ package com.saberhamza.config;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+/**
+ * 
+ * @author Hamaz SABER
+ *
+ */
 public class AppDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
+	/**
+	 * 
+	 * @return The configuration for root application context (non-webinfrastructure) configuration. 
+	 */
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
 		return null;
 	}
 
+	/**
+	 * 
+	 * @return The configuration for the Servlet application context.
+	 */
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
 		return new Class[] {AppConfig.class};
 	}
 
+	/**
+	 * 
+	 * @return Specify the servlet mapping(s) for DispatcherServletapplication context (Spring MVC infrastructure) configuration.
+	 */
 	@Override
 	protected String[] getServletMappings() {
 		return new String[] {"/"};
